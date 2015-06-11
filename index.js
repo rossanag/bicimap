be639d913431037a79459cc6ccd77a27dae35bfe
rossanag <guerra.rossana@gmail.com>
2015-06-11 02:00:19 -0300

    index y lala2

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ var res = [];
 var d = new Date();
 var h = d.getHours();
 
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+// server.listen(server_port, server_ip_address, function(){
+//   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+// });
+
 // Hora de de funcionamiento de las estaciones Plan Movete 7-21hs
 if ((h <= 21) && (h >= 7))
 {
